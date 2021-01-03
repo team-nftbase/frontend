4e432b05d70952bdaee8787c0ada62d3256ef45c
kuk <kuk@codev.kr>
2021-01-03 21:20:30 +0900

    tailwind 적용 완료

--- src/App.svelte
+++ src/App.svelte
@@ -1,41 +1,30 @@
 <script>
-	import { onMount } from "svelte";
-	let data;
-	let videoEl;
+	import Button from '@smui/button';
+	// import { onMount } from "svelte";
+	// let data;
+	// let videoEl;
 
-	onMount(async () => {
-		const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+	// onMount(async () => {
+	// 	const stream = await navigator.mediaDevices.getUserMedia({ video: true });
 
-		videoEl.srcObject = stream;
-		videoEl.play();
-	});
+	// 	videoEl.srcObject = stream;
+	// 	videoEl.play();
+	// });
 </script>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
 
 <main>
-	<h1 id="test">존나 좋군 asdadsa!</h1>
-	<video bind:this={videoEl} width="600" height="480">
-		<track kind="captions" />
-	</video>
+	<div class="flex flex-row justify-between">
+		<p>NFTBase</p>
+		<p>검색</p>
+		<Button on:click={() => alert('Clicked!')}>Just a Button</Button>
+		<div>
+			<a href="/">Explore</a>
+			<a href="/">F&Q</a>
+			<a href="/">Community</a>
+			<a href="/">Create</a>
+		</div>
+	</div>
 </main>
