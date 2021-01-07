679eb9b37072593ef9b238b8dc413d7fd42a4294
kuk-house <kuk@codev.kr>
2021-01-07 21:01:00 +0900

    [구현내용] - 검색기능 추가

--- src/pages/comp/explore/Collections.svelte
+++ src/pages/comp/explore/Collections.svelte
@@ -1,9 +1,8 @@
 <script>
-  import { each } from "svelte/internal";
-
   import { collections } from "../../store/explore.store";
 
   let collections_value;
+  let filter_text = "";
 
   const unsubscribe = collections.subscribe((value) => {
     collections_value = value;
@@ -20,10 +19,24 @@
 <div class="border-2 rounded-lg p-2 mr-2">
   <p class="text-xl font-bold">Collections</p>
   <hr />
+  <div class="flex flex-row px-4 py-2 my-2 rounded-lg border-2 border-gray-300">
+    <span class="material-icons mr-2"> search </span>
+    <input
+      class="border-transparent focus:outline-none w-full"
+      bind:value={filter_text}
+      placeholder="Search items, collections, and accounts" />
+    <button
+      class="flex items-center border-0 focus:outline-none"
+      on:click={() => {
+        filter_text = '';
+      }}><span class="material-icons"> clear </span></button>
+  </div>
   <div class="flex flex-col pt-2">
-    {#each Object.keys(collections_value) as collItem}
+    {#each Object.keys(collections_value).filter((item) =>
+      item.includes(filter_text)
+    ) as collItem}
       <button
-        class={`${collections_value[collItem] ? 'bg-gray-700 text-white' : ''} rounded-lg p-2 text-left focus:outline-none`}
+        class={`${collections_value[collItem] ? 'bg-gray-700 text-white' : ''} rounded-lg mb-2 p-2 text-left focus:outline-none`}
         on:click={(_) => handlerCollections(collItem)}>
         {collItem}
       </button>
