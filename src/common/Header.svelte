06177fad68e4ee28e262c89fd36e90b95587ae64
yujaejin <messheatup@gmail.com>
2021-03-20 14:30:46 +0900

    헤더 작업 (폰트제외)
    [구현내용]
    1. 헤더 작업 (폰트제외)

--- src/common/Header.svelte
+++ src/common/Header.svelte
@@ -16,53 +16,89 @@
 
 <div
   class="flex flex-col sm:flex-row justify-between items-center px-5 py-2"
-  id="header"
+  id="header_container"
 >
   <div class="flex flex-col w-full sm:w-auto sm:flex-row items-center">
     <Link to="/" class="text-blue-700 text-3xl font-extrabold"
-      ><img class="w-32" src="images/logo.png" alt="logoimage" /></Link
+      ><img
+        id="logo"
+        class="w-32"
+        src="images/logo.png"
+        alt="metamask_image"
+      /></Link
     >
     <div
-      class="flex flex-row px-4 py-2 w-full sm:w-96 rounded-lg border-2 border-gray-300"
+      id="search_container"
+      class="flex flex-row items-center px-4 py-2 w-full sm:w-96 rounded-lg border-2 border-gray-300"
     >
-      <img id="search_icon" src="images/search.png" alt="search_image" />
+      <img id="search_icon" src="images/search.png" alt="metamask_image" />
       <input
+        id="search_input"
         class="border-transparent focus:outline-none w-full"
         placeholder="Search by creator or collectable"
       />
     </div>
-  </div>
-  <div
-    class="flex flex-col sm:flex-row text-2xl font-bold text-black"
-    transition:slide
-  >
-    <Link to="explore"><p class="link">Explore</p></Link>
-    <Link to="fnq"><p class="link">F&Q</p></Link>
-    <Link to="community"><p class="link">Community</p></Link>
-    <Link to="create"><p class="link">Create</p></Link>
-    {#if userData.wallet}
-      <Link to="mypage">My page</Link>
-    {:else}
-      <Link
-        on:click={() => {
-          login();
-        }}><p class="link">login</p></Link
-      >
-    {/if}
+    <div
+      class="flex flex-col sm:flex-row text-2xl font-bold text-black"
+      transition:slide
+    >
+      <Link to="explore"><p>Explore</p></Link>
+      <Link to="fnq"><p>F&Q</p></Link>
+      <Link to="community"><p>Community</p></Link>
+      <Link to="create"><p>Create</p></Link>
+      {#if userData.wallet}
+        <Link to="mypage">My page</Link>
+      {:else}
+        <Link
+          on:click={() => {
+            login();
+          }}><p class="link">login</p></Link
+        >
+      {/if}
+    </div>
   </div>
 </div>
 
 <style>
-  #header {
+  #header_container {
     margin-left: 150px;
     margin-right: 150px;
   }
 
-  #search_icon {
-    width: 20.67px;
-    height: 21.33px;
+  #logo {
+    margin-right: 39px;
   }
-  .link {
+
+  #search_container {
+    width: 466px;
+    height: 57px;
+    left: 382px;
+    top: 69px;
+    background: #FFFFFF;
+    border : 0;
+    border-radius: 15px;
+    box-shadow: 0px 19px 35px rgba(115, 154, 176, 0.12);
+    /* box-shadow: 0px 19px 35px rgba(115, 154, 176, 0.12); */
+
+    margin-right: 37px;
+    padding-left : 44px;
+    padding-right : 0px;
+  }
+
+  #search_icon {
+    width: 31px;
+    height: 32px;
+    margin-right: 32px;
+  }
+
+  #search_input {
+    font-weight: 400;
+    font-style: normal;
+    font-size: 14px;
+    margin-right: 37px;
+  }
+
+  p {
     font-weight: 400;
     font-style: normal;
     font-size: 18px;
